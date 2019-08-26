8def11cbdfcb28a508c83bd3a995b86b3f4e47af
krlosexe <cardenascarlos18@gmail.com>
2019-08-25 19:22:19 -0500

    INSTRUCCIONES y DB

--- resources/views/users/new.blade.php
+++ resources/views/users/new.blade.php
@@ -10,7 +10,7 @@
                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                 <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus minlength="5">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="off" autofocus minlength="5">
                 </div>
             </div>
 
@@ -26,7 +26,7 @@
                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                 <div class="col-md-6">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeyup="validaPassword(this.value)">
+                    <input id="password" type="password"  class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeyup="validaPassword(this.value)">
                 </div>
             </div>
 
